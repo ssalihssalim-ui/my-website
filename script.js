5135b28886e6119cdd7b927017d1c19e2f24122d
ssalihssalim-ui <ssalihssalim@gmail.com>
2026-03-03 05:02:19 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuBtn = document.getElementById('menu-btn');
+    const closeBtn = document.getElementById('close-btn');
     const menu = document.getElementById('menu');
 
+    // Open menu
     menuBtn.addEventListener('click', () => {
-        menu.classList.toggle('show');
+        menu.classList.add('show');
+    });
+
+    // Close menu
+    closeBtn.addEventListener('click', () => {
+        menu.classList.remove('show');
     });
 });
