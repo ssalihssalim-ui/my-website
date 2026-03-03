0c1c098c444e63d29a8e4a14617ec445f31155af
ssalihssalim-ui <ssalihssalim@gmail.com>
2026-03-03 04:39:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// Get the menu button and menu
-const menuBtn = document.getElementById('menu-btn');
-const menu = document.getElementById('menu');
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.getElementById('menu-btn');
+    const menu = document.getElementById('menu');
 
-// Add click event
-menuBtn.addEventListener('click', function() {
-    // Toggle the 'show' class on the menu
-    menu.classList.toggle('show');
+    menuBtn.addEventListener('click', () => {
+        menu.classList.toggle('show');
+    });
 });
